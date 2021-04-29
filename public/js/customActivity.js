08590f006f3a158cba4344b2fef0e0049a308275
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-04-29 17:59:39 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -107,7 +107,7 @@ define([
           "headers": {
             "Content-Type": "application/json"
           },
-          "data": JSON.stringify({"to":"d045904a12ebea17187dcc","data":{"title":"Hello World POSTMAN!555","body":"Hello World!"},"notification":{"body":"Hello World ✌"}}),
+          "data": JSON.stringify({"to":"d045904a12ebea17187dcc","data":{"title":"Hello World Custom Activity","body":"Hello World!"},"notification":{"body":"Hello World ✌"}}),
         };
 
         $.ajax(settings).done(function (response) {
