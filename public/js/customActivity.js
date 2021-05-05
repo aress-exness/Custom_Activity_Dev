1061182b2098baa256f9a110302b499de0e0b050
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-05-05 13:17:35 +0530

    { "Title": "{{Event.25FF5ECF-6F3F-42E0-A0D9-45FC2C9AD60D.Title}}"},

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -94,8 +94,7 @@ define([
         
         payload['arguments'].execute.inArguments = [
 		{"tokens": authTokens},
-		// { "Title": "{{Event.25FF5ECF-6F3F-42E0-A0D9-45FC2C9AD60D.Title}}"},
-		{"ContactKey":"{{Contact.Key}}"},
+		{ "Title": "{{Event.25FF5ECF-6F3F-42E0-A0D9-45FC2C9AD60D.Title}}"},
 		   { "Data": "{{Contact.Attribute.PushyAPIDataDictionary.Data}}"},
 		   {"DeviceToken": "{{Contact.Attribute.PushyAPIDataDictionary.DeviceToken}}"}
             
