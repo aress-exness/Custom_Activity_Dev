7a5fe0587a7a76c7cacc397ee16e972c9798b7dc
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-05-07 19:19:19 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -65,7 +65,6 @@ exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
       console.log('on line 67');
-    alert('try');
     logData(req);
     res.send(200, 'Save');
 };
